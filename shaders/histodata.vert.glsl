0efd48a5e853780ee67fd826eef0daa1eba92ae6
toneDrei79 <tone0709@icloud.com>
2023-02-21 12:00:58 +0100

    change calculation of histodata.vert

--- shaders/histodata.vert.glsl
+++ shaders/histodata.vert.glsl
@@ -3,9 +3,14 @@ uniform vec3 color;
 uniform float roughness;
 
 void main() {
-    vec3 rgb = texture2D ( tex, position.xy ).rgb;
-    float pos = dot(color, rgb);
+    // vec3 rgb = texture2D (tex, position.xy).rgb;
+    // float pos = dot(color, rgb);
     
+    ivec2 texSize = textureSize(tex, 0);
+    ivec2 texelCoord = ivec2(gl_VertexID/texSize.x, gl_VertexID%texSize.x);
+    vec3 rgb = texelFetch(tex, texelCoord, 0).rgb;
+    float pos = dot(color, rgb);
+
     gl_Position = vec4(2.*pos - 1., 0., 0., 1.0);
     if (256.*pos < roughness) gl_PointSize = 256.*pos;
     else if (256.*pos > 256.-roughness) gl_PointSize = 256. - 256.*pos;
